29d2cb31de1a8cbd6eda17392a21bf64cf1946d4
newli <newlifechou.outlook.com>
2018-01-05 16:31:11 +0800

    create a project to test csvhelper

---
+++ TestCsvLib/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCsvLib
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ TestCsvLib/CsvHelpeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCsvLib
+{
+    public class CsvHelperTest
+    {
+        public void TestWriteSingle()
+        {
+
+        }
+    }
+}
