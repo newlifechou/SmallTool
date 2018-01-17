e37931843fb86a1e91cdadbc4b72e7e1cf2cbfb5
newli <newlifechou.outlook.com>
2018-01-17 17:35:10 +0800

    MassWriteWithTask

---
+++ GiantTextFileGenerator/MassOperation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace GiantTextFileGenerator
+{
+    class MassOperation
+    {
+        private string dataFilePath;
+        public MassOperation()
+        {
+            dataFilePath = Path.Combine(Environment.CurrentDirectory,  "MassData.txt");
+        }
+
+
+        public void WriteMassDataIntoFile()
+        {
+            var fs = new FileStream(dataFilePath, FileMode.Append);
+            var sw = new StreamWriter(fs);
+          
+            for (int i = 0; i < 100; i++)
+            {
+                Task t = new Task(() =>
+                {
+                    WriteMsg(sw);
+                });
+                t.Start();
+                t.Wait();
+            }
+            
+            sw.Close();
+            fs.Close();
+        }
+
+        private void WriteMsg(StreamWriter sw)
+        {
+            while (true)
+            {
+                string line = Guid.NewGuid().ToString();
+                sw.AutoFlush = true;
+                sw.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-{line}");
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ GiantTextFileGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiantTextFileGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("开始启动线程写入GUID到文件");
+            MassOperation operate = new MassOperation();
+            operate.WriteMassDataIntoFile();
+
+            Console.Read();
+        }
+    }
+}
