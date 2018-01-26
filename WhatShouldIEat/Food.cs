f4a5dde5a769c90ce98b405a5e47da3a80ba758a
newli <newlifechou.outlook.com>
2018-01-26 10:54:48 +0800

    add what should i eat project

---
+++ WhatShouldIEat/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatShouldIEat
+{
+    /// <summary>
+    /// 食物类
+    /// </summary>
+    class Food
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Characteristic { get; set; }
+    }
+}
---
+++ WhatShouldIEat/WhatToEat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatShouldIEat
+{
+    /// <summary>
+    /// 吃什么类
+    /// 这个类用来提供按条件选择食物的功能
+    /// 并提供自定义需求
+    /// </summary>
+    class WhatToEat
+    {
+        private List<Food> foods;
+        public WhatToEat()
+        {
+            foods = new List<Food>();
+        }
+
+
+        public int AddFood(Food food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateFood(Food food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteFood(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
