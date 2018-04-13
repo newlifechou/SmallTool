fd246b8e8860452079625b9f08a3b526f6bd7b05
newli <newlifechou.outlook.com>
2018-04-13 20:32:35 +0800

    create a random password generator

---
+++ RandomPassword/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomPassword
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            List<CharType> types = new List<CharType>();
+            if (CboNumber.IsChecked == true)
+            {
+                types.Add(CharType.Number);
+            }
+            if (CboUpper.IsChecked == true)
+            {
+                types.Add(CharType.Upper);
+            }
+            if (CboLower.IsChecked == true)
+            {
+                types.Add(CharType.Lower);
+            }
+            if (CboSpecial.IsChecked == true)
+            {
+                types.Add(CharType.Special);
+            }
+
+            int.TryParse(TxtLength.Text, out int length);
+
+            string password = new RandomPasswordGenerator().GetPassword(length, types.ToArray());
+
+            TxtPassword.Text = password;
+
+        }
+    }
+}
---
+++ RandomPassword/RandomPasswordGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPassword
+{
+
+    public class RandomPasswordGenerator
+    {
+        private string specialChar = @"~!@#$%^&*()_-+={}[]|\\<>/?";
+
+        public string GetPassword(int length, CharType[] types)
+        {
+            Random r = new Random();
+
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+
+                CharType currentType = types[r.Next(types.Length)];
+
+                switch (currentType)
+                {
+                    case CharType.Number:
+                        password.Append(r.Next(10));
+                        break;
+                    case CharType.Upper:
+                        password.Append((char)(r.Next(26) + 'A'));
+                        break;
+                    case CharType.Lower:
+                        password.Append((char)(r.Next(26) + 'a'));
+                        break;
+                    case CharType.Special:
+                        password.Append(specialChar[r.Next(specialChar.Length)]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            return password.ToString();
+        }
+
+
+
+
+    }
+}
